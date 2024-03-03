feb84a29ccce229492bde607085b0416c28b9844
syeasar <syeasar.kuet@gmail.com>
2024-03-03 19:19:08 +0600

    :construction: wip: build backend API for registration

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthProvider, useAuth } from './app/context/AuthContext';
+import { Button } from 'react-native-paper';
+import Login from './app/screens/Login';
+import Home from './app/screens/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <Layout></Layout>
+    </AuthProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export const Layout = ()=> {
+  const {authState, logout} = useAuth();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+{authState?.authenticated?(
+  <Stack.Screen name="Home" component={Home}
+  options={{
+    headerRight:()=><Button onPress={logout} title="Sign Out" />
+  }}
+  ></Stack.Screen>
+):(
+  <Stack.Screen name='Login' component={Login}></Stack.Screen>
+)}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
