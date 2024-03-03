c813f846d0f7850d2264efee0ec45f52e91d6f92
syeasar <syeasar.kuet@gmail.com>
2024-03-03 19:32:47 +0600

    :shirt: refactor: add code formatting

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { AuthProvider, useAuth } from './app/context/AuthContext';
-import { Button } from 'react-native-paper';
-import Login from './app/screens/Login';
-import Home from './app/screens/Home';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthProvider, useAuth } from "./app/context/AuthContext";
+import { Button } from "react-native-paper";
+import Login from "./app/screens/Login";
+import Home from "./app/screens/Home";
 
 const Stack = createNativeStackNavigator();
 
@@ -17,21 +17,23 @@ export default function App() {
   );
 }
 
-export const Layout = ()=> {
-  const {authState, logout} = useAuth();
+export const Layout = () => {
+  const { authState, logout } = useAuth();
   return (
     <NavigationContainer>
       <Stack.Navigator>
-{authState?.authenticated?(
-  <Stack.Screen name="Home" component={Home}
-  options={{
-    headerRight:()=><Button onPress={logout} title="Sign Out" />
-  }}
-  ></Stack.Screen>
-):(
-  <Stack.Screen name='Login' component={Login}></Stack.Screen>
-)}
+        {authState?.authenticated ? (
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerRight: () => <Button onPress={logout} title="Sign Out" />,
+            }}
+          ></Stack.Screen>
+        ) : (
+          <Stack.Screen name="Login" component={Login}></Stack.Screen>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
--- app/screens/Login.jsx
+++ app/screens/Login.jsx
@@ -1,65 +1,80 @@
 import { View, Image, Button, StyleSheet, TextInput } from "react-native";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { API_URL, useAuth } from "../context/AuthContext";
 
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, register } = useAuth();
 
-const Login = ()=>{
+  useEffect(() => {
+    const testCall = async () => {
+      const result = await axios.get(`${API_URL}/users`);
+      console.log("result[login:16]: ", result);
+    };
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const {login, register} = useAuth();
+    testCall();
+  }, []);
 
-    useEffect (()=>{
-        const testCall = async()=>{
-            const result = await axios.get(`${API_URL}/users`);
-            console.log('result[login:16]: ', result);
-        }
-
-        testCall();
-    }, [])    
-
-    const onLogin = async()=>{
-        const result = await login(email, password);
-        if(result && result.error) {
-            alert(result.msg);
-        }
+  const onLogin = async () => {
+    const result = await login(email, password);
+    if (result && result.error) {
+      alert(result.msg);
     }
+  };
 
-    const onRegister = async()=>{
-        const result = await register(email, password)
-        if(result && result.error) {
-            alert(result.msg);
-        }
-        else {
-            login();
-        }
+  const onRegister = async () => {
+    const result = await register(email, password);
+    if (result && result.error) {
+      alert(result.msg);
+    } else {
+      login();
     }
+  };
 
-    return(
-        <View>
-            <View style={styles.form}>
-                <TextInput styles={styles.input} placeholder="Email" onChangeText={(text)=>{setEmail(text)}} value={email} />
-                <TextInput styles={styles.input} placeholder="password" secureTextEntry = {true} onChangeText={(text)=>{setPassword(text)}} value={password} />
-                <Button onPress={onLogin} title="Sign In" />
-                <Button onPress={onRegister} title="Create Account" />
-            </View>
-        </View>
-    )
-}
-
+  return (
+    <View>
+      <View style={styles.form}>
+        <TextInput
+          styles={styles.input}
+          placeholder="Email"
+          onChangeText={(text) => {
+            setEmail(text);
+          }}
+          value={email}
+        />
+        <TextInput
+          styles={styles.input}
+          placeholder="password"
+          secureTextEntry={true}
+          onChangeText={(text) => {
+            setPassword(text);
+          }}
+          value={password}
+        />
+        <Button onPress={onLogin} title="Sign In" />
+        <Button onPress={onRegister} title="Create Account" />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    form: {
-        gap: 10,
-        width: '60'
-    },
-    input: {heigh:44,borderWidth:1, borderRadius:4, padding:10, backgroundColor: '#fff'},
-    container: {
-        alignItems: 'center',
-        width:'100'
-    }
-})
-
-
+  form: {
+    gap: 10,
+    width: "60",
+  },
+  input: {
+    heigh: 44,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  container: {
+    alignItems: "center",
+    width: "100",
+  },
+});
 
 export default Login;
